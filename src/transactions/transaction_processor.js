ef63ae853ffb4779fe4d2d804837b46f237994b3
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-02-28 16:13:31 +0000

    WORK IN PROGRES

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,44 +1,69 @@
-var txr = [];
-
 function processTransactions(transActions) {
 
-    // TEST GIT FORK+CLONE
-    txr = [];
+    let txr = [];
 
-    if(!validateTransactions(transActions)) {
-        throw new Error("Undefined collection of transactions")
+    if (transActions === undefined) throw new Error("Undefined collection of transactions");
+
+    // Reduce to count instances
+    let txCount = transActions.reduce((allTransactions, transaction) => 
+                                  (allTransactions[transaction] = allTransactions[transaction] + 1 || 1, allTransactions), {});
+
+
+
+    console.log(`before (txCount): ${txCount}`);
+    //
+    //txCount = sortByAmountThenName(txCount);
+
+    //const txCountCopy = Object.entries(txCount).sort(function sortingFunction(itemOne, itemTwo) {
+    //    return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)});
+    const txCountCopy = Object.entries(txCount).sort();
+
+    //const txCountCopy = Object.values(txCount).sort();
+    //const txCountCopy = Object.keys(txCount).sort();
+
+   //console.log(txr);
+    for (i=0;i<txCountCopy.length;i++) {
+        //txr[i] = `${txCountCopy[i][0]} ${txCountCopy[i][1]}`;
+        txr.push(`${txCountCopy[i][0]} ${txCountCopy[i][1]}`);
     }
+    console.log("After Sort ->" + txCountCopy + " Length "+ txCountCopy.length );
 
-    let txCount = {}
+//    console.log(`after (txCount): ${txCount}`);
 
-    const numberOfTransactions = transActions.length;
-
-    for(var i = 0; i < numberOfTransactions; i++) {
-        const transaction = transActions[i];
-        txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
-    }
-
-    txCount = sortByAmountThenName(txCount);
-    
     // Place them back in array for returning
-    Object.keys(txCount).forEach(function (key, index) {
-        txr[index] = `${key} ${txCount[key]}`;
-    });
+    // Object.keys(txCount).forEach(function (key, index) {
+    //     txr[index] = `${key} ${txCount[key]}`;
+    // });
 
     return txr;
 }
 
 function sortByAmountThenName(txCount) {
-    let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
-        return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
-    );
-
+    
+    
+    console.log(`Sort Object Entries ${Object.entries(txCount).sort()}`);
     let sortedResults = {};
-    for(let objectKey of sortedKeys) {
-        sortedResults[objectKey] = txCount[objectKey];
-    }
-
+    // let sortKeys = Object.entries(txCount).sort();
+    // for(let objectKey of sortKeys) {
+    //      sortedResults[objectKey] = txCount[objectKey];
+    // }
+    
     return sortedResults;
+
+    // console.log(`before (txCount): ${Object.entries(txCount)}`);
+    // let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
+    //     return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
+    // );
+    // console.log(`after (txCount): ${Object.entries(txCount)}`);
+    // console.log(`after (sortedKeys): ${sortedKeys}`);
+
+    // let sortedResults = {};
+    // for(let objectKey of sortedKeys) {
+    //     sortedResults[objectKey] = txCount[objectKey];
+    // }
+    // console.log(`after (sortedResults): ${Object.entries(sortedResults)}`);
+
+    // return sortedResults;
 }
 
 
