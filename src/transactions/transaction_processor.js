b8ce5294793e02304a86bfd076228797be45d1f7
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-03-01 09:35:55 +0000

    return mapped object key+count strings from sort

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,28 +1,18 @@
 function processTransactions(transActions) {
 
     // Cleanup VAR & error throw
-        let txr = [];
     if (transActions === undefined) throw new Error("Undefined collection of transactions");
 
     // REDUCE => get every trans from transActions, transAll inital blank object, index transAll object with transAction and increment from 1 upwards
     let txCount = transActions.reduce((transAll,trans) => (transAll[trans] = transAll[trans] + 1 || 1, transAll), {});
 
-        // Simply Sort
-        let sortedKeys = Object.keys(txCount).sort((itemOne, itemTwo) => txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo))
-
-        // Sorted count objects
-        let sortedResults = {};
-        for(let objectKey of sortedKeys) {
-            sortedResults[objectKey] = txCount[objectKey];
-        }
-        txCount = sortedResults;
-
-        // Place them back in array for returning
-        Object.keys(txCount).forEach(function (key, index) {
-            txr[index] = `${key} ${txCount[key]}`;
-        });
-
-        return txr;
+    // SORT parameters =>
+    // valueTwo > valueOne = POSTIVE (True) or NEGATIVE ( false) or ZERO (false) 
+    // itemOne > itemTwo = Alphabetical Comparison (First Greater Than Second = TRUE ) 
+    // -(itemOne < itemTwo) = Alphabetical Comparison (First Less Than Second = -1/FALSE)
+    // MAP => key followed by count
+    return Object.keys(txCount).sort((itemOne, itemTwo) => txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo))
+                               .map(key => `${key} ${txCount[key]}`);
 }
 
 module.exports = processTransactions;
