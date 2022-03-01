c35ff98862bbfbb36d3eef27490802869e4cb1a7
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-03-01 13:46:46 +0000

    arrow functions applied

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,4 +1,4 @@
-function processTransactions(transActions) {
+const processTransactions = (transActions) => {
 
     // Cleanup VAR & error throw
     if (transActions === undefined) throw new Error("Undefined collection of transactions");
@@ -7,13 +7,18 @@ function processTransactions(transActions) {
     let txCount = transActions.reduce((allTxr, transaction) => 
         ({ ...allTxr, [transaction]: allTxr[transaction] + 1 || 1}), {});
 
-    // SORT parameters =>
+    
+    let sortedKeys = sortByAmountThenName(txCount);
+
+    // Map key followed by count and return    
+    return sortedKeys.map(key => `${key} ${txCount[key]}`);
+}
+
+const sortByAmountThenName = (txCount) =>
+    // Sort parameters ->
     // valueTwo > valueOne = POSTIVE (True) or NEGATIVE ( false) or ZERO (false) 
     // itemOne > itemTwo = Alphabetical Comparison (First Greater Than Second = TRUE ) 
     // -(itemOne < itemTwo) = Alphabetical Comparison (First Less Than Second = -1/FALSE)
-    // MAP => key followed by count
-    return Object.keys(txCount).sort((itemOne, itemTwo) => txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo))
-                               .map(key => `${key} ${txCount[key]}`);
-}
+    Object.keys(txCount).sort((itemOne, itemTwo) => txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo))
 
 module.exports = processTransactions;
