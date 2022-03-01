6c7750a05f1ad97660e52241e36dd85113fdd808
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-03-01 09:32:10 +0000

    simply sort and bring function up (indented ready for replacing)

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,33 +1,28 @@
 function processTransactions(transActions) {
 
     // Cleanup VAR & error throw
-    let txr = [];
+        let txr = [];
     if (transActions === undefined) throw new Error("Undefined collection of transactions");
 
     // REDUCE => get every trans from transActions, transAll inital blank object, index transAll object with transAction and increment from 1 upwards
     let txCount = transActions.reduce((transAll,trans) => (transAll[trans] = transAll[trans] + 1 || 1, transAll), {});
 
-    txCount = sortByAmountThenName(txCount);
-    
-    // Place them back in array for returning
-    Object.keys(txCount).forEach(function (key, index) {
-        txr[index] = `${key} ${txCount[key]}`;
-    });
+        // Simply Sort
+        let sortedKeys = Object.keys(txCount).sort((itemOne, itemTwo) => txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo))
 
-    return txr;
-}
+        // Sorted count objects
+        let sortedResults = {};
+        for(let objectKey of sortedKeys) {
+            sortedResults[objectKey] = txCount[objectKey];
+        }
+        txCount = sortedResults;
 
-function sortByAmountThenName(txCount) {
-    let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
-        return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
-    );
+        // Place them back in array for returning
+        Object.keys(txCount).forEach(function (key, index) {
+            txr[index] = `${key} ${txCount[key]}`;
+        });
 
-    let sortedResults = {};
-    for(let objectKey of sortedKeys) {
-        sortedResults[objectKey] = txCount[objectKey];
-    }
-
-    return sortedResults;
+        return txr;
 }
 
 module.exports = processTransactions;
